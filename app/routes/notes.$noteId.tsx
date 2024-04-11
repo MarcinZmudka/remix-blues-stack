a7be569d1de38dc6186e0a042b7fba525811defb
Marcin <marcin.zmudka@brainhub.pl>
2024-04-11 13:56:54 +0200

    feat: add fetching data, error boundary, cache

--- app/routes/notes.$noteId.tsx
+++ app/routes/notes.$noteId.tsx
@@ -1,17 +1,41 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import {
+  isRouteErrorResponse,
+  useLoaderData,
+  useRouteError,
+} from "@remix-run/react";
+import invariant from "tiny-invariant";
 
-import { deleteNote, getNote } from "~/models/note.server";
+import { getNote } from "~/models/note.server";
 import { requireUserId } from "~/session.server";
 
 export const loader = async ({ params, request }: LoaderFunctionArgs) => {
-  return json({
-    note: {
-      title: "title",
-      body: "body",
-    },
+  const userId = await requireUserId(request);
+  invariant(params.noteId, "Note Id is required");
+
+  const note = await getNote({
+    id: params.noteId,
+    userId,
   });
+
+  if (!note) {
+    throw new Response("Note not found", {
+      status: 404,
+      statusText: "Note not found",
+    });
+  }
+
+  return json(
+    {
+      note,
+    },
+    {
+      headers: {
+        "Cache-Control": "public, max-age=360",
+      },
+    },
+  );
 };
 
 export default function NoteDetailsPage() {
@@ -25,3 +49,23 @@ export default function NoteDetailsPage() {
     </div>
   );
 }
+
+export const ErrorBoundary = () => {
+  const error = useRouteError();
+
+  if (!isRouteErrorResponse(error)) {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold mb-4">Unknown Error</h1>
+        <p className="text-gray-500">An unknown error occurred.</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold mb-4">{error.status}</h1>
+      <p className="text-gray-500">{error.statusText}</p>
+    </div>
+  );
+};
